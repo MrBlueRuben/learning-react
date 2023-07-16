a93042f47ca91f559e2f2a50f7dec3ad2346d5bf
ruben sanchez <sanchez.alvarez.ruben@gmail.com>
2023-07-16 16:08:29 +0200

    react basic component

---
+++ 01-twitter-follow-card/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+export function App() {
+  return (
+
+      <article className=''>
+        <header>
+          <img src="https://unavatar.io/github/mrBlueruben" alt="El avatar de Rubén Sánchez" />
+          <div>
+            <strong>Rubén Sánchez</strong>
+            <span>@MrBlueRuben</span>
+          </div>
+        </header>
+        <aside>
+          <button>Seguir</button>
+        </aside>
+      </article>
+  )
+}
